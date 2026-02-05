5dab5d089f99eb868c0b8a088d736e47c375b956
djumanov <djumanovdev@gmail.com>
2026-02-05 09:58:37 +0500

    product model

--- api/admin.py
+++ api/admin.py
@@ -1,11 +1,24 @@
 from django.contrib import admin
 from django.utils.html import format_html
 
-from .models import Category
+from .models import Category, Product
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'image')
+    list_display_links = ('id', 'name')
+    empty_value_display = '--empty--'
+    
+    @admin.display(description='Image')
+    def image(self, obj):
+        return format_html(
+            '<img src="http://localhost:8000/{}" width="50" height="50" style="object-fit: cover; border-radius: 4px;">',
+            obj.image.url
+        )
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'image')
     list_display_links = ('id', 'name')
     empty_value_display = '--empty--'
--- api/models.py
+++ api/models.py
@@ -4,8 +4,19 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
     description = models.TextField(blank=True, default='')
-    image = models.ImageField(upload_to="category-images/%Y/%m/%d/", blank=True, default="category-images/image.png")
+    image = models.ImageField(upload_to="category-images/%Y/%m/%d/", blank=True, default="image.png")
 
     def __str__(self):
         return self.name
     
+
+class Product(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    description = models.TextField(blank=True, default='')
+    image = models.ImageField(upload_to="product-images/%Y/%m/%d/", blank=True, default="image.png")
+    price = models.FloatField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
